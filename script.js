2061bc390faa1034e86215870e2c249772577676
aaeas <easley.aaron89@gmail.com>
2021-12-20 05:01:40 -0600

    added character selection details

--- script.js
+++ script.js
@@ -14,7 +14,16 @@ function generatePassword() {
     return null;
   }
 }
+var userInput = window.prompt("Which characters would you like to input? (Lowercase, Uppercase, Numeric, and/or Special Characters)")
+console.log("User entered: "+ userInput);
+const lowercaseletters = ['a', 'b', 'c',];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const alphaNumeric = letters.concat(numbers);
+console.log(alphaNumeric);
+// results in ['a', 'b', 'c', 1, 2, 3]
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 console.log(generateBtn);
 
