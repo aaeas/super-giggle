a618ba7431bfe20a0a81a27471ed50bc0fb4cf17
aaeas <easley.aaron89@gmail.com>
2021-12-20 07:28:53 -0600

    all window confirms added

--- script.js
+++ script.js
@@ -19,21 +19,42 @@ var userInput = window.confirm("Would you like capital letters?")
 console.log("User entered: "+ userInput);
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K' ];
 if (confirm('Are you sure you want to save this thing into the database?')) {
-  // Save it!
+  letters[Math.floor(Math.random() * letters.length)];
   console.log('Answer was saved to the database.');
 } else {
-  // Do nothing!
   console.log('Answer was not saved to the database.');
 }
 //Would you like lower case letters?
-
+var userInput = window.confirm("Would you like lower case letters?")
+console.log("User entered: "+ userInput);
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' ];
+if (confirm('Are you sure you want to save this thing into the database?')) {
+  letters[Math.floor(Math.random() * letters.length)];
+  console.log('Answer was saved to the database.');
+} else {
+  console.log('Answer was not saved to the database.');
+}
 //Would you like numbers?
+var userInput = window.confirm("Would you like numbers")
+console.log("User entered: "+ userInput);
+const letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
+if (confirm('Are you sure you want to save this thing into the database?')) {
+  letters[Math.floor(Math.random() * letters.length)];
+  console.log('Answer was saved to the database.');
+} else {
+  console.log('Answer was not saved to the database.');
+}
 
 //would you like special characters?
-const alphaNumeric = letters.concat(numbers);
-console.log(alphaNumeric);
-// results in ['a', 'b', 'c', 1, 2, 3]
-// Get references to the #generate element
+var userInput = window.confirm("Would you like capital letters?")
+console.log("User entered: "+ userInput);
+const letters = ['!', '@', '#', '$', '%', '^', '&', '*' ];
+if (confirm('Are you sure you want to save this thing into the database?')) {
+  letters[Math.floor(Math.random() * letters.length)];
+  console.log('Answer was saved to the database.');
+} else {
+  console.log('Answer was not saved to the database.');
+}
 
 var generateBtn = document.querySelector("#generate");
 console.log(generateBtn);
