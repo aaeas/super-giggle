97f85173ef6b0c38d25f0aa7ddc72f34855738f9
aaeas <easley.aaron89@gmail.com>
2021-12-20 21:53:52 -0600

    updated if statements

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Assignment code here
+var HasCapitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var HasLowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+var HasNumbers = "0123456789"
+var HasSpecialCharacters = "!@#$%^&*"
+
 function generatePassword() {
 
-  var myPass = "";
+  var myPass = '';
+  var passwordCharacterHolder = ''
 
   console.log("In Generate Password");
   var userCharacters = window.prompt("How many characters would you like? Choose between 8-128.")
@@ -18,50 +24,40 @@ function generatePassword() {
   }
   //Would you like capital letters?
   var userHasCapitalLetters = window.confirm("Would you like capital letters?")
-  console.log("User entered: " + userHasCapitalLetters);
-  const upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
-  // if (confirm('Are you sure you want to save this thing into the database?')) {
-  //   upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
-  //   console.log('Answer was saved to the database.');
-  // } else {
-  //   console.log('Answer was not saved to the database.');
-  // }
-  //Would you like lower case letters?
+  console.log("User entered: " + HasCapitalLetters);
+
+  //would you like lower case letters?
+
   var userHasLowerCaseLetters = window.confirm("Would you like lowercase letters?")
-  console.log("User entered: " + userHasLowerCaseLetters);
-  const lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
-  // if (confirm('Are you sure you want to save this thing into the database?')) {
-  //   lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
-  //   console.log('Answer was saved to the database.');
-  // } else {
-  //   console.log('Answer was not saved to the database.');
-  // }
-  //Would you like numbers?
+  console.log("User entered: " + HasLowerCaseLetters);
+
+  //would you like numbers?
+
   var userHasNumbers = window.confirm("Would you like numbers? (0-9) ")
-  console.log("User entered: " + userHasNumbers);
-  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  // if (confirm('Are you sure you want to save this thing into the database?')) {
-  //   numbers[Math.floor(Math.random() * numbers.length)];
-  //   console.log('Answer was saved to the database.');
-  // } else {
-  //   console.log('Answer was not saved to the database.');
-  // }
+  console.log("User entered: " + HasNumbers);
 
   //would you like special characters?
 
   var userHasSpecialCharacters = window.confirm("Would you like special characters? (!, @, #, $, %, ^, &, *)")
-  console.log("User entered: " + userHasSpecialCharacters);
-  const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*'];
-  // if (confirm('Are you sure you want to save this thing into the database?')) {
-  //   specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-  //   console.log('Answer was saved to the database.');
-  // } else {
-  //   console.log('Answer was not saved to the database.');
-  // }
+  console.log("User entered: " + HasSpecialCharacters);
 
-  for (let i = 0; i < userCharacters; i++) {
+  //if they want a character then add it to the myPass variable
+  if (HasCapitalLetters) {
+    passwordCharacterHolder += HasCapitalLetters
   }
-
+  if (HasLowerCaseLetters) {
+    passwordCharacterHolder += HasLowerCaseLetters
+  }
+  if (HasNumbers) {
+    passwordCharacterHolder += HasNumbers
+  }
+  if (userHasSpecialCharacters) {
+    passwordCharacterHolder += HasSpecialCharacters
+  }
+  for (var i = 0; i < userCharacters; i++) {
+    //adding a character at random from our passwordcharacter holder
+    myPass += passwordCharacterHolder.charAt(Math.floor(Math.random() * passwordCharacterHolder.length))
+  }
 
   return myPass;
 }
